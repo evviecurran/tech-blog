b45f64a2cd0c6814469da5faf67f3f048e1028da
evviecurran <125322606+evviecurran@users.noreply.github.com>
2023-09-09 11:16:30 -0600

    fixed

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 
 
 const sequelize = process.env.JAWSDB_URL 
-? new Squelize(process.env.JAWSDB_URL)
+? new Sequelize(process.env.JAWSDB_URL)
 
 : new Sequelize(
     process.env.DB_NAME,
