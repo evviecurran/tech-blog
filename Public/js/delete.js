b8360ff3d16147b122ab84b0b9c7ed0a64b002cd
evviecurran <125322606+evviecurran@users.noreply.github.com>
2023-10-08 14:55:14 -0600

    debugging

--- Public/js/delete.js
+++ Public/js/delete.js
@@ -13,9 +13,6 @@ const deletePostHandler = async function(event) {
 }
 
 document.querySelector("#delete-btn").addEventListener("click", deletePostHandler);
-
-
-// $(".delete-blog-button").click(async function (event) {
 //     event.preventDefault();
 //     const res = await fetch (`/api.blog/${$(e.target).data("id")}`,{
 //         method: "DELETE",
