001230cc5b4650defc2bf6e86a5181fbdcbbd339
evviecurran <125322606+evviecurran@users.noreply.github.com>
2023-10-08 20:23:01 -0600

    debugging

--- models/index.js
+++ models/index.js
@@ -1,27 +1,53 @@
-import User from './User';
-import { belongsTo, hasMany } from './Post';
-import Comment, { belongsTo as _belongsTo } from './Comment';
+// import User from './User';
+// import { belongsTo, hasMany } from './Post';
+// import Comment, { belongsTo as _belongsTo } from './Comment';
 
-belongsTo(User, {
+// belongsTo(User, {
+//     foreignKey: 'userId',
+//     onDelete: 'CASCADE'
+// });
+
+// hasMany(Comment, {
+//     foreignKey: 'postId',
+//     onDelete: 'CASCADE'
+// });
+
+// _belongsTo(User, {
+//     foreignKey: 'userId',
+//     onDelete: 'CASCADE'
+// });
+
+// export default {
+//     User, 
+//     Comment, 
+//     Post
+// };
+
+
+
+
+
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+Post.belongsTo(User, {
     foreignKey: 'userId',
     onDelete: 'CASCADE'
 });
 
-hasMany(Comment, {
+Post.hasMany(Comment, {
     foreignKey: 'postId',
     onDelete: 'CASCADE'
 });
 
-_belongsTo(User, {
+Comment.belongsTo(User, {
     foreignKey: 'userId',
     onDelete: 'CASCADE'
 });
 
-export default {
+module.exports = {
     User, 
     Comment, 
     Post
 };
-
-
-
