0c3e7edb0cfca28fc09c5bd42ec04407319e2188
evviecurran <125322606+evviecurran@users.noreply.github.com>
2023-10-11 18:54:18 -0600

    adding in user not username

--- config/connection.js
+++ config/connection.js
@@ -4,14 +4,34 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // creates connection to db either local or heroku that uses jawsdb
-const sequelize = process.env.JAWSDB_URL
+const sequelize = process.env.JAWSDB_URL 
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
+    user: 'root',
     dialect: 'mysql',
     port: 3306
   });
 
+// module.exports = sequelize;
+
+
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+    
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306
+//     }
+//   );
+// }
+
 module.exports = sequelize;
-
-
